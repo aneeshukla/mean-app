425c6e6a206beda78ab2ed6c892c145fc73b6811
Aneesh Shukla <Aneesh.Shukla@pb.com>
2018-12-28 11:55:19 +0530

    added toolbar and expansion panel

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+
+  constructor() { }
+
+  enteredVal:string;
+  
+  onAddPost(){
+    
+  }
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+  // posts = [
+  //   {title: "First post", content: "First post ka content"},
+  //   {title: "Second post", content: "2nd post ka content"},
+  //   {title: "Third post", content: "3rd post ka content"},
+  // ];
+  posts=[];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
