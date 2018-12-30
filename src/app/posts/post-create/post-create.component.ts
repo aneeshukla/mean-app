b8453fb95bbe5992dfc24e03d5046f2d46817d10
Aneesh Shukla <aneeshshukla@outlook.com>
2018-12-30 20:37:29 +0530

    added handling for empty post submission

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 import { Post } from '../post.model';
 
 @Component({
@@ -14,10 +16,13 @@ export class PostCreateComponent implements OnInit {
   enteredContent= "";
   @Output() postCreated = new EventEmitter<Post>();
   
-  onAddPost(){
+  onAddPost(form: NgForm){
+    if(form.invalid){ //checking to see if form has content
+      return;
+    }
     const post:Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
+      title: form.value.title,
+      content: form.value.content
     };
     this.postCreated.emit(post);
   }
